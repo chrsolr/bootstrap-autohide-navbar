1c9b7566b11af06e6720827373243442620fff3f
iamrelos <twenty40@gmail.com>
2017-07-24 20:43:51 -0500

    updated

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+const gulp = require('gulp');
+const glp = require('gulp-load-plugins')({
+    lazy: true
+});
+
+gulp.task('build', () => {
+    const minify_opts = {
+        ext: {
+            min: '.min.js'
+        },
+        ignoreFiles: ['-min.js', '.min.js'],
+        mangle: true
+    };
+
+    return gulp.src('./src/bootstrap-navbar.js')
+        .pipe(glp.minify(minify_opts))
+        .pipe(gulp.dest(`./dist/`));
+});
+
+gulp.task('default', ['build']);
